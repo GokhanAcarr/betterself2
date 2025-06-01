bdc059e72706796c8286ddc49c06fa87ed602d36
MrKaratash <keremkaratas03@gmail.com>
2025-06-01 13:45:38 +0300

    Sadece pythonProject klasörü eklendi

--- pythonProject/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User, WaterIntake, SleepRecord
-from .food import UserFoodLog
-from .post import Post
-from .exercise import Exercise
-from .program import UserCustomProgram, UserCustomProgramExercise, UserDailyProgramAssignment
